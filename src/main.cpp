8867bdd141d1cbdf891bfb6e6329839b71b66119
paarthmahadik <paarth.mahadik@gmail.com>
2026-09-06 00:46:01 +0530

    got a square on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,21 @@ const int height = 600;
 
 void framebuffer_callback(GLFWwindow *window, int width, int height);
 
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main(){\n"
+    "	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 int main()
 {
     glfwInit();
@@ -29,9 +44,62 @@ int main()
     }
     glfwSetFramebufferSizeCallback(window, framebuffer_callback);
 
+    GLuint vertexshader;
+    vertexshader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexshader, 1, &vertexShaderSource, null);
+    glCompileShader(vertexshader);
+
+    GLuint fragmentshader;
+    fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentshader, 1, &fragmentShaderSource, null);
+    glCompileShader(fragmentshader);
+
+    GLuint shaderprogram;
+    shaderprogram = glCreateProgram();
+    glAttachShader(shaderprogram, vertexshader);
+    glAttachShader(shaderprogram, fragmentshader);
+    glLinkProgram(shaderprogram);
+
+    glDeleteShader(vertexshader);
+    glDeleteShader(fragmentshader);
+
+    float vertices[] = {
+        0.5f,  0.5f,  0.0f, // top right
+        0.5f,  -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f,  0.0f  // top left
+    };
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    GLuint vbo, vao, ebo;
+    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.0f, 0.0f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderprogram);
+        glBindVertexArray(vao);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
