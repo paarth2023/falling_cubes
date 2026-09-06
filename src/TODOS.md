3dced746512bf39caf712fe5d563fb0e005be88f
paarthmahadik <paarth.mahadik@gmail.com>
2026-09-06 15:57:10 +0530

    added textures

---
+++ src/TODOS.md
@@ -0,0 +1,5 @@
+# TODOS 
+
+## 6-9-26
+
+- [ ] add glm to the mix.
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,38 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "stb_image.h"
 
 #define null NULL
 
-const int width = 800;
-const int height = 600;
+const int sWidth = 800;
+const int sHeight = 600;
 
 void framebuffer_callback(GLFWwindow *window, int width, int height);
 
-const char *vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "layout (location = 2) in vec2 aTexCoord;\n"
+                                 "out vec3 ourColor;\n"
+                                 "out vec2 TexCoord;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos, 1.0);\n"
+                                 "	ourColor = aColor;\n"
+                                 "	TexCoord = aTexCoord;\n"
+                                 "}\0";
 
 const char *fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n"
+    "in vec2 TexCoord;\n"
+    "uniform sampler2D texture1;\n"
+    "uniform sampler2D texture2;\n"
     "void main(){\n"
-    "	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "	FragColor = mix(texture(texture1, TexCoord), texture(texture2, "
+    "TexCoord), 0.2);\n"
     "}\n\0";
 
 int main()
@@ -31,7 +42,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     std::cout << "Hello world" << std::endl;
     GLFWwindow *window =
-        glfwCreateWindow(width, height, "Hello, World!", null, null);
+        glfwCreateWindow(sWidth, sHeight, "Hello, World!", null, null);
     if (window == NULL) {
         std::cout << "Could not create window" << std::endl;
         glfwTerminate();
@@ -65,10 +76,11 @@ int main()
     glDeleteShader(fragmentshader);
 
     float vertices[] = {
-        0.5f,  0.5f,  0.0f, // top right
-        0.5f,  -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f,  0.0f  // top left
+        // positions          // colors           // texture coords
+        0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
     };
     unsigned int indices[] = {
         // note that we start from 0!
@@ -76,26 +88,79 @@ int main()
         1, 2, 3  // second triangle
     };
 
-    GLuint vbo, vao, ebo;
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("resources/solid_color_512x512.png", &width,
+                                    &height, &nrChannels, 0);
+    std::cout << nrChannels << std::endl;
+
+    GLuint vbo, vao, ebo, texture1, texture2;
     glGenBuffers(1, &vbo);
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &ebo);
+    glGenTextures(1, &texture1);
+    glGenTextures(1, &texture2);
 
     glBindVertexArray(vao);
 
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                     GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Error loading texture1" << std::endl;
+    }
+    stbi_image_free(data);
+
+    data = stbi_load("resources/solid_color_512x512.png", &width, &height,
+                     &nrChannels, 0);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                     GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                  GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                           (void *)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glUseProgram(shaderprogram);
+    glUniform1i(glGetUniformLocation(shaderprogram, "texture1"), 0);
+    glUniform1i(glGetUniformLocation(shaderprogram, "texture2"), 1);
 
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.0f, 0.0f, 0.3f, 1.0f);
+        glClearColor(0.3f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
         glUseProgram(shaderprogram);
         glBindVertexArray(vao);
         // glDrawArrays(GL_TRIANGLES, 0, 3);
